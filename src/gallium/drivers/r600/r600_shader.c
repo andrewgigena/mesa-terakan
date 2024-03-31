4ad7445ef1de97af9ecd727cfee8c73f623197c7
Vitaliy Triang3l Kuzmin <triang3l@yandex.ru>
2024-03-31 19:10:53 +0000

    r600: Use C99 variadic macros in r600_pipe_common.h
    GNU variadic macros are not supported by MSVC.
    
    Signed-off-by: Vitaliy Triang3l Kuzmin <triang3l@yandex.ru>

--- src/gallium/drivers/r600/r600_shader.c
+++ src/gallium/drivers/r600/r600_shader.c
@@ -152,13 +152,13 @@ int r600_pipe_shader_create(struct pipe_context *ctx,
 	int processor = sel->ir_type == PIPE_SHADER_IR_TGSI ?
 		tgsi_get_processor_type(sel->tokens):
 		pipe_shader_type_from_mesa(sel->nir->info.stage);
-	
+
 	bool dump = r600_can_dump_shader(&rctx->screen->b, processor);
 
 	unsigned export_shader;
-	
+
 	shader->shader.bc.isa = rctx->isa;
-	
+
 	{
 		glsl_type_singleton_init_or_ref();
 		if (sel->ir_type == PIPE_SHADER_IR_TGSI) {
@@ -184,26 +184,26 @@ int r600_pipe_shader_create(struct pipe_context *ctx,
 
 		if (r) {
 			fprintf(stderr, "--Failed shader--------------------------------------------------\n");
-			
+
 			if (sel->ir_type == PIPE_SHADER_IR_TGSI) {
 				fprintf(stderr, "--TGSI--------------------------------------------------------\n");
 				tgsi_dump(sel->tokens, 0);
 			}
-			
+
 			fprintf(stderr, "--NIR --------------------------------------------------------\n");
 			nir_print_shader(sel->nir, stderr);
-			
+
 			R600_ERR("translation from NIR failed !\n");
 			goto error;
 		}
 	}
-	
+
 	if (dump) {
 		if (sel->ir_type == PIPE_SHADER_IR_TGSI) {
 			fprintf(stderr, "--TGSI--------------------------------------------------------\n");
 			tgsi_dump(sel->tokens, 0);
 		}
-		
+
 		if (sel->so.num_outputs) {
 			r600_dump_streamout(&sel->so);
 		}
@@ -582,14 +582,14 @@ static int emit_streamout(struct r600_shader_ctx *ctx, struct pipe_stream_output
 
 	/* Sanity checking. */
 	if (so->num_outputs > PIPE_MAX_SO_OUTPUTS) {
-		R600_ERR("Too many stream outputs: %d\n", so->num_outputs);
+		R600_ERR_F("Too many stream outputs: %d\n", so->num_outputs);
 		r = -EINVAL;
 		goto out_err;
 	}
 	for (i = 0; i < so->num_outputs; i++) {
 		if (so->output[i].output_buffer >= 4) {
-			R600_ERR("Exceeded the max number of stream output buffers, got: %d\n",
-				 so->output[i].output_buffer);
+			R600_ERR_F("Exceeded the max number of stream output buffers, got: %d\n",
+			           so->output[i].output_buffer);
 			r = -EINVAL;
 			goto out_err;
 		}
@@ -1021,4 +1021,3 @@ int generate_gs_copy_shader(struct r600_context *rctx,
 
 	return r600_bytecode_build(ctx.bc);
 }
-
