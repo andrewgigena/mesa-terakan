7408bda026bbbcf5b5ff0bbbed01d55b2d83e954
Vitaliy Triang3l Kuzmin <triang3l@yandex.ru>
2024-06-09 17:18:39 +0300

    r600/sfn: Implement mbcnt_amd
    Signed-off-by: Vitaliy Triang3l Kuzmin <triang3l@yandex.ru>

--- src/gallium/drivers/r600/sfn/sfn_shader.h
+++ src/gallium/drivers/r600/sfn/sfn_shader.h
@@ -320,6 +320,7 @@ private:
    bool emit_local_store(nir_intrinsic_instr *intr);
    bool emit_local_load(nir_intrinsic_instr *instr);
    bool emit_load_tcs_param_base(nir_intrinsic_instr *instr, int offset);
+   bool emit_mbcnt(nir_intrinsic_instr *instr);
    bool emit_group_barrier(nir_intrinsic_instr *intr);
    bool emit_shader_clock(nir_intrinsic_instr *instr);
    bool emit_wait_ack();
